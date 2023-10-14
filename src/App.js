96d88f59280e8c4af8fd49b3f124968c3127140b
Sushi-Samurai <122423970+Sushi-Samurai@users.noreply.github.com>
2023-10-13 17:42:27 -0700

    Built weather app
    A lot of troubleshooting and referencing from StackOverflow. Very heavy influence from instructor solution.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import React from "react";
-import Weather from "./Weather";
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import Navigation from "./Navigation";
+import Main from "./Main";
 
-import "./App.css";
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello World</h1>
-        <Weather city="Tokyo" />
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navigation />
+        <Main />
+      </div>
+    );
+  }
 }
-
-export default App;
--- src/Weather.js
+++ src/Weather.js
@@ -1,26 +1,119 @@
-import React from "react";
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 import axios from "axios";
+import "./Weather.css";
+import WeatherIcon from "./WeatherIcon";
+import CurrentLocation from "./CurrentLocation";
+import Search from "./Search";
+import DateUtil from "./DateUtil";
+import Api from "./Api";
+import Forecast from "./Forecast";
+
 import { ThreeDots } from "react-loader-spinner";
 
-export default function Weather(props) {
-  function handleResponse(response) {
-    alert(
-      `The weather in ${response.data.name} is ${response.data.main.temp}°F`
-    );
+export default class Weather extends Component {
+  static propTypes = {
+    city: PropTypes.string.isRequired,
+  };
+
+  state = {
+    city: this.props.city,
+  };
+
+  componentWillMount() {
+    this.refresh(this.state.city);
   }
-  let apiKey = "ebef9ca4a8de66ed586fac628fade056";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=imperial`;
-  axios.get(apiUrl).then(handleResponse);
-  return (
-    <ThreeDots
-      height="80"
-      width="80"
-      radius="9"
-      color="turquoise"
-      ariaLabel="three-dots-loading"
-      wrapperStyle={{}}
-      wrapperClassName=""
-      visible={true}
-    />
-  );
+
+  refreshWeatherFromParams(params) {
+    let url = `${Api.url}/data/2.5/weather?appid=${Api.key}&units=metric&${params}`;
+    axios.get(url).then((response) => {
+      this.setState({
+        city: response.data.name,
+        weather: {
+          description: response.data.weather[0].main,
+          icon: response.data.weather[0].icon,
+          precipitation: Math.round(response.data.main.humidity) + "%",
+          temperature: Math.round(response.data.main.temp),
+          time: new DateUtil(new Date(response.data.dt * 1000)).dayTime(),
+          wind: Math.round(response.data.wind.speed) + "km/h",
+        },
+      });
+    });
+  }
+
+  refreshWeatherFromLatitudeAndLongitude = (latitude, longitude) => {
+    this.refreshWeatherFromParams(`lat=${latitude}&lon=${longitude}`);
+  };
+
+  refresh = (city) => {
+    this.refreshWeatherFromParams(`q=${city}`);
+  };
+
+  render() {
+    if (this.state.weather) {
+      return (
+        <div>
+          <div className="clearfix">
+            <Search refresh={this.refresh} />
+            <CurrentLocation
+              refresh={this.refreshWeatherFromLatitudeAndLongitude}
+            />
+          </div>
+
+          <div className="weather-summary">
+            <div className="weather-summary-header">
+              <h1>{this.state.city}</h1>
+              <div className="weather-detail__text">
+                {this.state.weather.time}
+              </div>
+              <div className="weather-detail__text">
+                {this.state.weather.description}
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col-sm-6">
+                <div className="clearfix">
+                  <div className="float-left weather-icon">
+                    <WeatherIcon iconName={this.state.weather.icon} />
+                  </div>
+                  <div className="weather-temp weather-temp--today">
+                    {this.state.weather.temperature}
+                  </div>
+                  <div className="weather-unit__text weather-unit__text--today">
+                    °C
+                  </div>
+                </div>
+              </div>
+              <div className="col-sm-6">
+                <div className="weather-detail__text">
+                  Precipitation: {this.state.weather.precipitation}
+                </div>
+                <div className="weather-detail__text">
+                  Wind: {this.state.weather.wind}
+                </div>
+              </div>
+            </div>
+          </div>
+          <Forecast city={this.state.city} />
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          App is loading, <em>please wait...</em>
+          <ThreeDots
+            height="80"
+            width="80"
+            radius="9"
+            color="turquoise"
+            ariaLabel="three-dots-loading"
+            wrapperStyle={{}}
+            wrapperClassName=""
+            visible={true}
+          />
+        </div>
+      );
+    }
+  }
 }
