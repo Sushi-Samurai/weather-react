e70c7c786c46680e9cad223e60a34343aa7ef004
Sushi-Samurai <122423970+Sushi-Samurai@users.noreply.github.com>
2023-10-17 18:01:38 -0700

    added bootstrap to project

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDom from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.css";
 import App from "./App";
 
 ReactDom.render(
